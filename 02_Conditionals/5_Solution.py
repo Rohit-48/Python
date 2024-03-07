77522967266e2d3ef822ee975c3a5aaf562a51bc
Rohit Mandavkar <rohitmandavkar3577@gmail.com>
2024-03-07 11:43:49 +0530

    Refactor weather activity logic

--- 02_Conditionals/5_Solution.py
+++ 02_Conditionals/5_Solution.py
@@ -1,8 +1,10 @@
 weather = "Rainy"
 
 if weather == "Sunny":
-    print("Go for a walk")
+    activity = "Go for a walk"
 elif weather == "Rainy":
-    print(" Read a book")
+    activity = " Read a book"
 elif weather == "Snowy":
-    print("Build a snowman")
+    activity = "Build a snowman"
+
+print(activity)
