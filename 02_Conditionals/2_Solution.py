c6f1bcc1520688055fcc94f2917d59e07dcdbd95
Rohit Mandavkar <rohitmandavkar3577@gmail.com>
2024-03-07 11:13:29 +0530

    Add solution for conditional ticket pricing

---
+++ 02_Conditionals/2_Solution.py
@@ -0,0 +1,11 @@
+
+input_age = int(input("Enter Your Age: ") ) 
+
+input_day = input("Enter Today Day: ")
+
+price = 12 if input_age >= 18 else 8
+
+if input_day == "Wednesday":
+     price -= 2
+
+print("Ticket price for you is: $", price)
