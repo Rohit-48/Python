8d86526a59bc00a82343b811a3b6c941e26ca288
Rohit Mandavkar <rohitmandavkar3577@gmail.com>
2024-03-07 11:28:01 +0530

    Refactor conditional statements in 02_Conditionals/3_Solution.py

--- 02_Conditionals/3_Solution.py
+++ 02_Conditionals/3_Solution.py
@@ -1,12 +1,16 @@
 score = int(input("Enter Your Score: "))
 
+if score >= 101:
+    print("Please Valid Score")
+    exit()
+
 if score >= 90:
     print("Your Grade is A")
-elif 80 <= score <= 89:
+elif  score >= 80:
     print("Your Grade is B")
-elif 70 <= score <= 79:
+elif  score  >= 70:
     print("Your Grade is C")
-elif 60 <= score <= 69:
+elif  score  >= 60:
     print("Your Grade is D")
 else:
     print("Your Grade is F")
