7176c7be1503df5c631df346d011344dee16ddcf
Rohit Mandavkar <rohitmandavkar3577@gmail.com>
2024-03-14 10:59:19 +0530

    Add loop to validate user input for a number between 1-10

---
+++ 03_Loops/7_Solution.py
@@ -0,0 +1,10 @@
+while True:
+    number = int(input("Enter a number Between 1-10:"))
+    if 1 <= number <= 10:
+       print("Thanks")
+       break
+       
+    else:
+        print("Invalid Number, Enter Number Between 1-10:")
+
+       
